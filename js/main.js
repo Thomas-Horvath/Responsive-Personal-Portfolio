b014a88d94fcec8f78f6ff42b8aeffa91d7812b0
Thomas-Horvath <thomas.horvath0416@gmail.com>
2023-10-21 13:18:45 +0200

    portfolio section

--- js/main.js
+++ js/main.js
@@ -14,3 +14,22 @@ toggleBtn.addEventListener("click", () => {
     toggleBtn.children[1].classList.toggle("displayBlock");
 });
 
+//portfolio
+
+ let allProjectThumbnail = document.querySelectorAll(".thumbnail");
+ function setProjectThumbnails(allProjectThumbnail, projectName, color){
+    allProjectThumbnail.style =`
+    background: url( ../Assets/ProjectThumbnails/${projectName}.png) no-repeat center;
+    background-size: contain;
+    background-color: ${color};
+    `;
+ } 
+ setProjectThumbnails(allProjectThumbnail[0], "project1", "#27272c");
+ setProjectThumbnails(allProjectThumbnail[1], "project2", "#27272c");
+ setProjectThumbnails(allProjectThumbnail[2], "project3", "#863286");
+ setProjectThumbnails(allProjectThumbnail[3], "project4", "#000");
+ setProjectThumbnails(allProjectThumbnail[4], "project5", "#fff");
+ setProjectThumbnails(allProjectThumbnail[5], "project6", "#202020");
+ setProjectThumbnails(allProjectThumbnail[6], "project7", "#8339ec");
+ setProjectThumbnails(allProjectThumbnail[7], "project8", "#272727");;
+ setProjectThumbnails(allProjectThumbnail[8], "project9", "#272727");
